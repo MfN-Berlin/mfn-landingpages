3e25b8e3a902f4c5975085cc40292489d3429f99
maxlisewski <hey@maxlisewski.de>
2024-11-25 10:50:56 +0100

    v0.0.90

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ const IndexPage = () => {
 
         <Section backgroundColor="bg-white" columns={1}>
           <CardText
-            headline="mfn-landingpages v0.0.89"
+            headline="mfn-landingpages v0.0.90"
             headlineStyle="h1"
             body="interne Startseite, hier sind die bereits angelegten Landingpages und Zusatzseiten"
             spacing="wide"
