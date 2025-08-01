dbfc651e107bb292a39e6e6e8652cd4e5e12f970
Max Lisewski <hey@maxlisewski.de>
2025-08-01 08:16:24 +0200

    1.0.30 added version number

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,7 +7,7 @@
 import './src/styles/global.css'
 
 // Version logging
-const VERSION = 'v1.0.25'
+const VERSION = 'v1.0.30'
 
 export const onClientEntry = () => {
     // Temporär Prefetching deaktivieren
