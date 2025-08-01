bcfd9e1735bc77f2817c2c844cc9d9a09fdd96a7
Max Lisewski <hey@maxlisewski.de>
2025-08-01 08:24:06 +0200

    updated closing times

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,7 +7,7 @@
 import './src/styles/global.css'
 
 // Version logging
-const VERSION = 'v1.0.30'
+const VERSION = 'v1.0.31'
 
 export const onClientEntry = () => {
     // Temporär Prefetching deaktivieren
